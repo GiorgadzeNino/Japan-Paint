4540b6300f040012f436f206a3365339818fbb5c
nino-coder <nino.giorgadze.2000@gmail.com>
2020-12-05 17:50:04 +0400

    change functional

--- javascript/nonogram_solver.js
+++ javascript/nonogram_solver.js
@@ -1,58 +1,77 @@
 // const colHints.length = document.getElementById('colHints.length');
 // const rowHints.length = document.getElementById('rowHints.length');
+const EASY_PERCENT = 92;
+const MEDIUM_PERCENT = 82;
+const HARD_PERCENT = 72;
+const EASY_DIFFICULTY = 'easy';
+const MEDIUM_DIFFICULTY = 'medium';
+const HARD_DIFFICULTY = 'hard';
+const STATE_UNSELECTED = 0;
+const STATE_SELECTED = 1;
+const STATE_MARKED = 2;
+const DEFAULT_DIFFICULTY = MEDIUM_DIFFICULTY;
+
 const sizeError = document.getElementById('sizeError');
 const colHints = document.getElementById('column-hints');
 const rowHints = document.getElementById('row-hints');
 const picture = document.getElementById('picture');
 console.log(picture);
-
-// User inputs
-// NOTE: Initial values are mostly for testing.
-//       5x5 grid is ok for production, but I think the hints should be empty to start
-const userInputs = {
-
-    colHints: [
-        [2, 2],
-        [2, 3],
-        [2, 4],
-        [5, 2],
-        [4, 1, 1],
-        [5, 2],
-        [2, 4],
-        [2, 3],
-        [2, 2]
-
-    ],
-    rowHints: [
-        [5],
-        [9],
-        [2, 3, 2],
-        [3],
-        [2, 2],
-        [2, 1, 2],
-        [4, 4],
-        [9]
-    ]
-}
-console.log(`Initial userInputs:`);
-console.log(userInputs);
-
+let game = {
+        colHints: [],
+        rowHints: [],
+        solution: []
+    }
+    // User inputs
+    // NOTE: Initial values are mostly for testing.
+    //       5x5 grid is ok for production, but I think the hints should be empty to start
+    // start()
+console.log(`Initial game:`);
+console.log(game);
+let column;
+let row;
 // Initial puzzle draw
-drawPuzzle();
+// drawPuzzle();
 
+let showTable = false;
+
+document.getElementById('start-easy').addEventListener('click',
+    () => {
+        showTable = true;
+        start(EASY_DIFFICULTY);
+        document.getElementById("start-game").disabled = false;
+        startGame();
+    });
+document.getElementById('start-medium').addEventListener('click',
+    () => {
+        showTable = true;
+        start(MEDIUM_DIFFICULTY);
+        document.getElementById("start-game").disabled = false;
+        startGame();
+    });
+document.getElementById('start-hard').addEventListener('click',
+    () => {
+        showTable = true;
+        start(HARD_DIFFICULTY);
+        document.getElementById("start-game").disabled = false;
+        startGame();
+    });
+document.getElementById('start-game').addEventListener('click',
+    () => {
+        startGame();
+    });
 // Catch when the user types a value into one of the size inputs
 const handleSizeInput = e => {
         console.log(`Saw input change to ${e.target.id}: ${e.target.value}`);
         if (Number(e.target.value)) {
             sizeError.innerText = "";
-            userInputs[e.target.id] = e.target.value;
-            console.log(`Change userInputs to:`);
-            console.log(userInputs);
+            game[e.target.id] = e.target.value;
+            console.log(`Change game to:`);
+            console.log(game);
             drawPuzzle();
         } else {
             console.log(`Input is not a number`);
             sizeError.innerText = "Invalid input! Only numbers allowed.";
-            e.target.value = userInputs[e.target.id];
+            e.target.value = game[e.target.id];
         }
     }
     // colHints.length.onchange = handleSizeInput;
@@ -77,31 +96,32 @@ function drawColHints() {
 
     // Iterate over the hints, creating elements along the way
     console.log('Starting column loop');
-    console.log(userInputs.colHints)
-    for (col = 0; col < userInputs.colHints.length; col++) {
+    console.log(game.colHints)
+    for (col = 0; col < game.colHints.length; col++) {
         let currCol = colHints.appendChild(document.createElement('div'));
         currCol.id = `hintsCol${col}`;
         currCol.classList.add('hint-group');
         console.log('Starting element loop');
-        console.log(userInputs.colHints[col]);
-        console.log(userInputs.colHints[col].length);
+        console.log(game.colHints[col]);
+        console.log(game.colHints[col].length);
 
         // Special case if the column hints is an empty array
-        if (userInputs.colHints[col].length == 0) {
+        if (game.colHints[col].length == 0) {
             currHint = currCol.appendChild(document.createElement('div'));
             currHint.id = `hintCol${col}Blank`;
             currHint.classList.add('element', 'blank');
         }
         // Otherwise iterate over the elements in column hints
         else {
-            for (elm = 0; elm < userInputs.colHints[col].length; elm++) {
+            for (elm = 0; elm < game.colHints[col].length; elm++) {
                 currHint = currCol.appendChild(document.createElement('div'));
                 currHint.id = `hintCol${col}Elm${elm}`;
                 currHint.classList.add('element');
-                currHint.innerText = userInputs.colHints[col][elm];
+                currHint.innerText = game.colHints[col][elm];
             }
         }
     }
+    this.column = col;
 }
 
 // Function to redraw the row hints
@@ -112,31 +132,32 @@ function drawRowHints() {
 
     // Iterate over the hints, creating elements along the way
     console.log('Starting row loop');
-    console.log(userInputs.rowHints)
-    for (row = 0; row < userInputs.rowHints.length; row++) {
+    console.log(game.rowHints)
+    for (row = 0; row < game.rowHints.length; row++) {
         let currRow = rowHints.appendChild(document.createElement('div'));
         currRow.id = `hintsRow${row}`;
         currRow.classList.add('hint-group');
         console.log('Starting element loop');
-        console.log(userInputs.rowHints[row]);
-        console.log(userInputs.rowHints[row].length);
+        console.log(game.rowHints[row]);
+        console.log(game.rowHints[row].length);
 
         // Special case if the row hints is an empty array
-        if (userInputs.rowHints[row].length == 0) {
+        if (game.rowHints[row].length == 0) {
             currHint = currRow.appendChild(document.createElement('div'));
             currHint.id = `hintRow${row}Blank`;
             currHint.classList.add('element', 'blank');
         }
         // Otherwise iterate over the elements in row hints
         else {
-            for (elm = 0; elm < userInputs.rowHints[row].length; elm++) {
+            for (elm = 0; elm < game.rowHints[row].length; elm++) {
                 currHint = currRow.appendChild(document.createElement('div'));
                 currHint.id = `hintRow${row}Elm${elm}`;
                 currHint.classList.add('element');
-                currHint.innerText = userInputs.rowHints[row][elm];
+                currHint.innerText = game.rowHints[row][elm];
             }
         }
     }
+    this.row = row;
 }
 
 // Function to redraw the picture grid
@@ -145,11 +166,11 @@ function drawGrid() {
     picture.innerHTML = "";
 
     // Iterate over rows and columns, creating elements along the way
-    for (rowY = 0; rowY < userInputs.rowHints.length; rowY++) {
+    for (rowY = 0; rowY < game.rowHints.length; rowY++) {
         let currRow = picture.appendChild(document.createElement('div'));
         currRow.id = `pictureRow${rowY}`;
         currRow.classList.add('picture-row');
-        for (colX = 0; colX < userInputs.colHints.length; colX++) {
+        for (colX = 0; colX < game.colHints.length; colX++) {
             currCol = currRow.appendChild(document.createElement('div'));
             currCol.id = `pictureCol${colX}`;
             currCol.classList.add('element');
@@ -157,20 +178,210 @@ function drawGrid() {
     }
 }
 
+var black = false;
+var userSolution = new Array(game.solution.length);
+console.log(userSolution)
+for (var i = 0; i < userSolution.length; i++) {
+    userSolution[i] = 0;
+}
+
+
+function checkWinner() {
+    if (JSON.stringify(game.solution) == JSON.stringify(userSolution)) {
+        console.log("You Won!");
+        document.write("YOU WON")
+    } else {
+        console.log("no")
+    }
+    console.log(game.solution);
+    console.log(userSolution);
+}
+
+//  function some() {
+
+
+
+
+//////                           gaferadeba
+window.onclick = e => {
+    // debugger
+    if (e.target.classList.contains("element")) {
+        var x = e.target.id.slice(-1); //abrunebs  strings 
+        var y = e.target.parentNode.id.slice(-1); //abrunebs strings
+        var index = game.colHints.length * Number(y) + Number(x);
+        console.log(index)
+        if (!black) {
+
+            userSolution[index] = 1;
+            e.target.style.background = "black"
+            this.black = true;
+        } else {
+
+            userSolution[index] = 0;
+            e.target.style.background = "white"
+            this.black = false;
+        }
+    }
+    checkWinner();
+}
+
+// for (let i = 0; i < elements.length; i++) {
+//     el = elements[i];
+
+// el.addEventListener('click', function (event) {
+//     alert('Hi!');
+// });
+// }
 $(document).ready(function() {
+    // debugger
+    // $('.level').click(function() {
+    //     boxId = $(this).attr('id');
+    // });
+    // console.log(boxId)
+
     var black = false;
     $(".element").click(function() {
+        var x = parseInt($(this)[0].id.slice(-1));
+        var y = parseInt($(this).parent()[0].id.slice(-1));
+        var index = col * y + x;
         if (!this.black) {
+            // debugger
             $(this).css('background', 'black');
             this.black = true;
+            userSolution[index] = 1;
+
         } else {
             $(this).css('background', 'white');
             this.black = false;
+            userSolution[index] = 0;
         }
+        checkWinner();
     })
-    for (let column = 0; column > userInputs.colHints.length; column++) {
-        // for(userInputs.colHints)
-    }
+
 
 });
-// document.addEventListener("click", printMousePos);
+// }
+// function cl() {
+//     $(document).ready(function() {
+//         $(document).click(function(event) {
+//             var parentDiv = $(event.target).closest("div");
+//             console.log(parentDiv.prop("id"));
+//         });
+//     });
+
+// }
+
+
+function startGame() {
+    // debugger
+    document.getElementById("start-game").disabled = false;
+    drawPuzzle();
+    drawGrid();
+    // setHints();
+    // reset();
+}
+
+function start(difficulty) {
+    switch (difficulty) {
+        case EASY_DIFFICULTY:
+            percentSpots = EASY_PERCENT;
+            game = {
+                colHints: [
+                    [2, 2],
+                    [2, 3],
+                    [2, 4],
+                    [5, 2],
+                    [4, 1, 1],
+                    [5, 2],
+                    [2, 4],
+                    [2, 3],
+                    [2, 2]
+
+                ],
+                rowHints: [
+                    [5],
+                    [9],
+                    [2, 3, 2],
+                    [3],
+                    [2, 2],
+                    [2, 1, 2],
+                    [4, 4],
+                    [9]
+                ],
+                solution: [
+                    0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1,
+                    1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1,
+                    0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+                    1, 1, 1, 1, 1, 1, 1, 1
+                ]
+            }
+            break;
+        case MEDIUM_DIFFICULTY:
+            percentSpots = MEDIUM_PERCENT;
+            game = {
+                colHints: [
+                    [5, 2],
+                    [2, 3],
+                    [2, 4],
+                    [5, 2],
+                    [4, 1, 1],
+                    [5, 2],
+                    [2, 4],
+                    [2, 3],
+                    [2, 2]
+
+                ],
+                rowHints: [
+                    [5],
+                    [9],
+                    [2, 3, 2],
+                    [3],
+                    [2, 2],
+                    [2, 1, 2],
+                    [4, 4],
+                    [9]
+                ],
+                solution: [
+                    1, 0, 1, 0, 1, 0, 1, 0
+                ]
+            }
+            break;
+        case HARD_DIFFICULTY:
+            console.log(game)
+            percentSpots = HARD_PERCENT;
+            game = {
+                colHints: [
+                    [6, 2],
+                    [2, 3],
+                    [2, 4],
+                    [5, 2],
+                    [4, 1, 1],
+                    [5, 2],
+                    [2, 4],
+                    [2, 3],
+                    [2, 2]
+
+                ],
+                rowHints: [
+                    [5],
+                    [9],
+                    [2, 3, 2],
+                    [3],
+                    [2, 2],
+                    [2, 1, 2],
+                    [4, 4],
+                    [9]
+                ],
+                solution: [
+                    1, 0, 1, 0, 1, 0, 1, 0
+                ]
+            }
+            break;
+        default:
+            start(DEFAULT_DIFFICULTY);
+            break;
+    }
+
+
+}
